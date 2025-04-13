8fc10f6701fcad62ae288e77c556a09e5d129d47
Corki <oraen1998@gmail.com>
2025-04-13 19:12:51 +0800

    标题头跳转

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "antd";
 import logo from "@/assets/images/xn.png";
 import { useStyle } from "./style"
+import {useState} from "react";
 interface LayoutHeaderProps {
   children: JSX.Element | null
 }
@@ -10,10 +11,21 @@ const { Header } = Layout;
 
 const LayoutHeader = ({ children }: LayoutHeaderProps) => {
   const { styles } = useStyle()
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <Header className={styles.header}>
-      <div className="logo">
-        <img src={logo} alt="logo"></img>
+      <div
+        className="logo"
+        onClick={() => window.open("https://oraen.com", "_blank")}
+        style={{
+          cursor: "pointer",
+          color: isHovered ? "#FFC107" : "inherit", // 悬停时琥珀黄
+          transition: "color 0.3s ease",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img src={logo} alt="logo"/>
         <span>牙膏盒 | oraen.com</span>
       </div>
     </Header>
