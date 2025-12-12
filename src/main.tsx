6a1f6003d5f561975c91b35d1781a19f630bdf16
Corki <oraen1998@gmail.com>
2025-12-13 03:00:22 +0800

    v2.1.1 修复electron下无法使用vs editor的问题

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from './App'
 import "./assets/css/global"
 
 // 配置 Monaco Editor 使用本地文件而不是 CDN
-loader.config({ paths: { vs: '/vs' } });
+// 判断是否是 Electron 中运行（会使用 file:// 协议）
+const isElectron = window.location.protocol === 'file:';
+const vsPaths = isElectron 
+  ? `${window.location.pathname.replace(/\/index.html$/, '')}/vs`
+  : '/vs';
+
+loader.config({ paths: { vs: vsPaths } });
 
 createRoot(document.getElementById('root') as Element).render(<App />)
