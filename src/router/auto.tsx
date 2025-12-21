ee7e4987bd3c45d79a6962f21fa3c51caf05e1d2
Corki <oraen1998@gmail.com>
2025-12-21 19:16:17 +0800

    v2.2.1-1 提交auto.tsx

---
+++ src/router/auto.tsx
@@ -0,0 +1,45 @@
+// 本文件为脚本自动生成，请勿修改
+
+
+
+const routes = [{
+  [MENU_PATH]: "/about/overview",
+  components: () => import("../pages/about/overview/index.js")
+}, {
+  [MENU_PATH]: "/baseUtil/timeUtil",
+  components: () => import("../pages/baseUtil/timeUtil/index.js")
+}, {
+  [MENU_PATH]: "/baseUtil/textUtil",
+  components: () => import("../pages/baseUtil/textUtil/index.js")
+}, {
+  [MENU_PATH]: "/baseUtil/mathUtil",
+  components: () => import("../pages/baseUtil/mathUtil/index.js")
+}, {
+  [MENU_PATH]: "/baseUtil/cryptologyUtil",
+  components: () => import("../pages/baseUtil/cryptologyUtil/index.js")
+}, {
+  [MENU_PATH]: "/baseUtil/langUtil",
+  components: () => import("../pages/baseUtil/langUtil/index.js")
+}, {
+  [MENU_PATH]: "/about/other",
+  components: () => import("../pages/about/other/index.js")
+}, {
+  [MENU_PATH]: "/fileUtil/conversion",
+  components: () => import("../pages/fileUtil/conversion/index.js")
+}, {
+  [MENU_PATH]: "/fileUtil/encryption",
+  components: () => import("../pages/fileUtil/encryption/index.js")
+}, {
+  [MENU_PATH]: "/professionalUtil/colorUtil",
+  components: () => import("../pages/professionalUtil/colorUtil/index.js")
+}, {
+  [MENU_PATH]: "/professionalUtil/qrcodeUtil",
+  components: () => import("../pages/professionalUtil/qrcodeUtil/index.js")
+}, {
+  [MENU_PATH]: "/professionalUtil/apiDebugTool",
+  components: () => import("../pages/professionalUtil/apiDebugTool/index.js")
+}];
+
+
+
+export default routes
